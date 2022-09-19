b3907076cb2deee323c0d5189a2efd0450b4ecae
Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2022-09-19 15:37:37 +0200

    Working Version
    ❌Removed:
    > unused code 
    
    ✔️ Updated:
    > fixed editor code
    > updated example page
    
    📔 More Info on (Readme): https://github.com/Klubuntu/myol-editor/blob/master/README.md

--- js/myol-editor.js
+++ js/myol-editor.js
@@ -1,88 +1,55 @@
-// First Version
+// Working Version
 // myol-editor (MyOneList Editor)
 // Copyright 2022 (c), All rights reserved
 // Created by Klubuntu
 var SelectedText;
 
-//On Yesterdays Progress
-// Work Functions from Toolbox (bold, italic, underline: etc:)
 
-function changeTag(elem, newTag) {
-    newEl = document.createElement(newTag);
-    newEl.innerHTML = elem.innerHTML;
-    elem.parentNode.replaceChild(newEl, elem);
-}
+function switchTag(tag) {
 
-function switchTag(tag){
-
-// https://codepen.io/saigowthamr/pen/OZmWqW
-// https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand
-// https://levelup.gitconnected.com/using-design-mode-and-execcommand-to-fiddle-with-web-pages-6f4039f7f406
+    // https://codepen.io/saigowthamr/pen/OZmWqW
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand
+    // https://levelup.gitconnected.com/using-design-mode-and-execcommand-to-fiddle-with-web-pages-6f4039f7f406
 
     // Headers
 
-    if (tag == "h1"){
+    if (tag == "h1") {
         document.execCommand('formatBlock', false, '<' + tag + '>');
     }
-    else if (tag == "h2"){
+    else if (tag == "h2") {
         document.execCommand('formatBlock', false, '<' + tag + '>');
     }
-    else if (tag == "h3"){
+    else if (tag == "h3") {
         document.execCommand('formatBlock', false, '<' + tag + '>');
     }
-    else if (tag == "h4"){
+    else if (tag == "h4") {
         document.execCommand('formatBlock', false, '<' + tag + '>');
     }
-    else if (tag == "h5"){
+    else if (tag == "h5") {
         document.execCommand('formatBlock', false, '<' + tag + '>');
     }
-    else if (tag == "h6"){
+    else if (tag == "h6") {
         document.execCommand('formatBlock', false, '<' + tag + '>');
     }
 
     // Format Text
 
-    if (tag == "b"){
+    if (tag == "b") {
         document.execCommand('bold', false, '');
-    }    
-    else if (tag == "u"){
+    }
+    else if (tag == "u") {
         document.execCommand('underline', false, '');
-    }    
-    else if (tag == "i"){
+    }
+    else if (tag == "i") {
         document.execCommand('italic', false, '');
     }
 
 
 }
 
-
-function replaceSelectedText(replacementText) {
-    var sel, range;
-    if (window.getSelection) {
-        sel = window.getSelection();
-        if (sel.rangeCount) {
-            range = sel.getRangeAt(0);
-            range.deleteContents();
-            range.insertNode(document.createTextNode(replacementText));
-        }
-    } else if (document.selection && document.selection.createRange) {
-        range = document.selection.createRange();
-        range.text = replacementText;
-    }
-}
-
-function BoldTextEditor(elm){
-    var highlight = window.getSelection();  
-     
-    var span = '<span style="font-weight:bold;">' + highlight + '</span>';
-     
-    document.querySelector(elm).value = document.querySelector(elm).value.replace(new RegExp(highlight , 'g'), span);
-}
-
-
 function buildMyOLEditor(elm, options) {
     $style = "<style type='text/css'>@import url('css/external/myol-text.css'); .textarea::-webkit-scrollbar { width: 0 !important }\
-    .textarea{width: 328px;height: 278px;background: transparent;border: 0;outline: none;}\
+    .textarea{width: 313px;height: 282px;background: transparent;border: 0;outline: none;margin-left: 9px;display: inline-block;overflow: hidden;}\
     .editor{width: 330px;height: 330px;background: antiquewhite;border-radius: 8px;}\
     .toolbox img{width: 11.5%;;display:inline-flex;}\
     .toolbox img:hover{background: #c1bcbcc4;border-radius: 4px;}\
@@ -97,11 +64,56 @@ function buildMyOLEditor(elm, options) {
     <img src="js/img/h3.svg" class="h3" alt="h3"/>\
     <img src="js/img/h4.svg" class="h4" alt="h4"/>\
     <img src="js/img/h5.svg" class="h5" alt="h5"/>\
-    </div><p class="textarea" contenteditable="true"></p></span></div>';
+    </div><p class="textarea" contenteditable="true"></p></span></div><script type="text/javascript">buildButtons()</script>';
     document.querySelector(elm).innerHTML = $style + $editorCode;
+    buildButtons();
 }
 
-document.addEventListener('selectionchange', (e)=>{
+document.addEventListener('selectionchange', (e) => {
     //console.log("Archor node - ",window.getSelection().anchorNode);
     SelectedText = window.getSelection().toString();
 })
+
+/* Add Events to buttons */
+function buildButtons() {
+    document.querySelectorAll('.bold').forEach(occurence => {
+        occurence.addEventListener('click', (e) => {
+            switchTag("b")
+        });
+    });
+    document.querySelectorAll('.italic').forEach(occurence => {
+        occurence.addEventListener('click', (e) => {
+            switchTag("i")
+        });
+    });
+    document.querySelectorAll('.underline').forEach(occurence => {
+        occurence.addEventListener('click', (e) => {
+            switchTag("u")
+        });
+    });
+    document.querySelectorAll('.h1').forEach(occurence => {
+        occurence.addEventListener('click', (e) => {
+            switchTag("h1")
+        });
+    });
+    document.querySelectorAll('.h2').forEach(occurence => {
+        occurence.addEventListener('click', (e) => {
+            switchTag("h2")
+        });
+    });
+    document.querySelectorAll('.h3').forEach(occurence => {
+        occurence.addEventListener('click', (e) => {
+            switchTag("h3")
+        });
+    });
+    document.querySelectorAll('.h4').forEach(occurence => {
+        occurence.addEventListener('click', (e) => {
+            switchTag("h4")
+        });
+    });
+    document.querySelectorAll('.h5').forEach(occurence => {
+        occurence.addEventListener('click', (e) => {
+            switchTag("h5")
+        });
+    });
+}
